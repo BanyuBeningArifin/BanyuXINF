3a43987c1e35ffe42e193e7c7c842f27c366c51a
BanyuBeningArifin <banyuba12@gmail.com>
2025-05-09 18:57:54 +0700

    Operator

---
+++ ope.py
@@ -0,0 +1,24 @@
+# Penjumlahan
+print(2 + 3)
+pear = 6
+mangga = 8
+buah = pear + mangga
+print(buah)
+
+# Pengurangan
+tabungan = 10000
+keluaran = 5000
+sisaTabungan = tabungan - keluaran
+print("Sisa tabungan Anda adalah", sisaTabungan)
+
+# Perkalian
+panjang = 15
+lebar = 4
+luas = panjang * lebar
+print(luas)
+
+# Pembagian
+kue = 5
+anak = 5
+bagian = kue // anak
+print(bagian)
