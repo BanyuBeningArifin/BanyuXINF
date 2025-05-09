49b4e92347242ce97d13c3f153c8e3d68e7bc697
BanyuBeningArifin <banyuba12@gmail.com>
2025-05-09 18:57:25 +0700

    Variabel

---
+++ var.py
@@ -0,0 +1,26 @@
+#data
+nama = "Desmond"
+
+#cetak variabel
+print(nama)
+
+#nilai dan tipe data
+umur = 21                     
+print(umur)                             
+
+umur = "dua puluh satu"       
+print(umur)                  
+            
+
+namaDepan = "Banyu"
+namaBelakang = "Bening"
+nama = namaDepan + " " + namaBelakang
+umur = 16
+hobi = "Berenang"
+
+print("Biodata:\n", nama, "\n", umur, "\n", hobi)
+
+#variabel lain
+inivariabel = "Halo"
+ini_juga_variabel = "Hai"
+_inivariabeljuga = "Hi"
