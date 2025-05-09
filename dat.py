2da635af23dabb2edbad24f5f11cc42119d79731
BanyuBeningArifin <banyuba12@gmail.com>
2025-05-09 18:56:42 +0700

    Tipe Data

---
+++ dat.py
@@ -0,0 +1,23 @@
+#tipe data Boolean
+print(True)
+
+#tipe data String
+print("Jangan Takut Belajar Python")
+print('Belajar Python Itu Lebih Baik')
+
+#tipe data Integer
+print(-20)
+
+#tipe data Float
+print(3.14)
+
+#tipe data Hexadecimal
+print("Bentuk hexadesimal dari 23 adalah " + hex(23))
+print("nama saya : " + "bagus")
+
+#tipe data Complex
+print(5j)
+
+#tipe data List
+print([1,2,3,4,5])
+print(["satu", "dua", "tiga"])
